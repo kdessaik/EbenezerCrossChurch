c5e8aa63f93f8c8bd512fa00b442788b0c562993
Kambale Kibeho <dessaikibeho@gmail.com>
2022-11-26 20:04:33 +0300

    allChange

--- src/Header/NavBar/NavBar.js
+++ src/Header/NavBar/NavBar.js
@@ -1,56 +1,136 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Logo from "../../images/logo.jpg"
-import "./NavBar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../images/logo.jpg";
+import "./NavBar.css";
 
-export default function Navbar () {
-  const NavPhone=()=>{
-  
-  }
- 
+export default function Navbar() {
+  const NavPhone = () => {
+    
+    
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" id='NavBar'>
-      
-  <a className="navbar-brand" href="#"><img src={Logo} className="Logo1"  alt='Loading '/> <h1> <span>Ebenezer</span> <span>Cross</span> <span>Church</span> </h1></a>
-  <button className="navbar-toggler" type="button" datatoggle="collapse"  datatarget="#navbarNavDropdown" ariac-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" onClick={NavPhone}>
-    <span className="navbar-toggler-icon" id='navCollapse'>
-
-
-      
-    </span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul className="navbar-nav">
-      <li className="nav-item active ">
-  
-        <Link className="nav-link" to='/' >Home <span class="sr-only">(current)</span></Link>
-      </li>
-      <li className="nav-item">
-       
-        <Link className="nav-link" to='/extension'>Extensions</Link>
-      </li>
-      <li className="nav-item">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light" id="NavBar">
+      <a className="navbar-brand" href="#">
+        <img src={Logo} className="Logo1" alt="Loading " />{" "}
+        <h1>
+          {" "}
+          <span>Ebenezer</span> <span>Cross</span> <span>Church</span>{" "}
+        </h1>
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        datatoggle="collapse"
+        datatarget="#navbarNavDropdown"
+        ariac-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={NavPhone}
+      >
         
-        <Link className="nav-link" to='/program'>Programs</Link>
-      </li>
-      <li className="nav-item">
-      <Link className="nav-link" to='/online'>Online</Link>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Helping</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Media</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Contact</a>
-      </li>
-      <li className="nav-item btn btn-primary">
-        <a className="nav-link" id='btn' href="#">Offerings</a>
-    </li>
-    </ul>
-  </div>
+        
+        <div class="">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+  <span className="navbar-toggler-icon" id="navCollapse1">
+          
+  <ul class="dropdown-menu">
+
+          <li className="nav-item active ">
+            <Link className="nav-link" to="/">
+              Home <span class="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/extension">
+              Extensions
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/program">
+              Programs
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/online">
+              Online
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Helping
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Media
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+          <li className="nav-item btn btn-primary" id="btn-primary">
+            <a className="nav-link" id="btn" href="#">
+              Offerings
+            </a>
+          </li>
+        
+  </ul>
+          </span>
+  </button>
   
-</nav>
-  )
+</div>
+          
+        
+        
+
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active ">
+            <Link className="nav-link" to="/">
+              Home <span class="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/extension">
+              Extensions
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/program">
+              Programs
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/online">
+              Online
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Helping
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Media
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+          <li className="nav-item btn btn-primary">
+            <a className="nav-link" id="btn" href="#">
+              Offerings
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
